cba9eb36fafc836ab2d535c8820e717c0683659a
AndersonConne <andyconne56@gmail.com>
2025-04-18 14:12:32 -0500

    add basename to browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./components/App/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/se_project_final">
       <App />
     </BrowserRouter>
   </StrictMode>
